6f2b11e78bcb90291890c911442d69cc2c31019c
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-05-18 16:34:49 +0530

    commands installation started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:please run with root access"
+    exit 1 #give other than 0 upto 127 bcz 0 means success exit status
 else
     echo "You are running with root access, proceed to install"
 fi
