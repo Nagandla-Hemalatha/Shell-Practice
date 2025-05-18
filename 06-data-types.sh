49a8d6a162fad5095d0b1ffc284f47c273e856db
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-05-18 11:35:01 +0530

    data types started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -2,6 +2,13 @@
 
 #it indicates Comment
 
-TIMESTAMP=$(date)
+#TIMESTAMP=$(date)
 
-echo "This script emtered on : $TIMESTAMP"
+#echo "This script emtered on : $TIMESTAMP"
+
+NUMBER1=100
+NUMBER2=400
+
+SUM=(($NUMBER1+$NUMBER2))
+
+echo "Sum of $NUMBER1 and $NUMBER2 is : $SUM"
