3c7192033f03b72bb1066ee47ca3ee40d5b1eca2
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-05-25 11:24:21 +0530

    set started

--- 17-set.sh
+++ 17-set.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+failure(){
+    echo "Failed at :$1 $2"
+}
+
+trap 'failure "${LINENO}" "${BASH_COMMAND}" ' ERR
 echo "Hello world!"
 echoooo "Hell, this will be error"
 echo "Hello world"
