836be069e80a317d615a7a46ac6ddcb19c89324c
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-05-27 17:45:58 +0530

    email started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -18,4 +18,4 @@ done <<< $DISK_USAGE
 
 #echo -e $MSG
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "khajadevi@gmail.com" "ALERT-High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "khajadevi849@gmail.com" "ALERT-High Disk Usage"
