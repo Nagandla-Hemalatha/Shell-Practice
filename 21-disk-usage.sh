2422f40305ea38d42566be931641c5a090ef08c0
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-05-28 10:50:41 +0530

    email started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,23 +1,23 @@
 
 # #!/bin/bash
 
-# DISK_USAGE=$(df -hT | grep -v Filesystem)
-# DISK_THRESHOLD=1 # in project it will be 75
-# MSG=""
-# IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # in project it will be 75
+MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-# while IFS= read line
-# do
-#     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-#     PARTITION=$(echo $line | awk '{print $7F}')
-#     if [ $USAGE -ge $DISK_THRESHOLD ]
-#     then
-#         MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
-#     fi
-# done <<< $DISK_USAGE
+while IFS= read line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
+    fi
+done <<< $DISK_USAGE
 
-# #echo -e $MSG
+#echo -e $MSG
 
-# sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "b.raviteja5555@gmail.com" "ALERT-High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "khajadevi849@gmail.com" "ALERT-High Disk Usage"
 
-sh mail.sh
+#sh mail.sh
